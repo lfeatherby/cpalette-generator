0ee9301682a5eee53b15c426f2ec6955ff327f28
lfeatherby <larissafeatherby@gmail.com>
2024-10-31 14:30:44 -0400

    updated gradient picker box labels
    labels now change on input in the text boxes

--- gradient/gradScript.js
+++ gradient/gradScript.js
@@ -7,6 +7,8 @@ var pickerE = new iro.ColorPicker('#pickerE', {
 let sel = document.getElementById('sel');
 let b1 = document.getElementById('box1');
 let b2 = document.getElementById('box2');
+let lbl1 = document.getElementById('lbl1');
+let lbl2 = document.getElementById('lbl2');
 let offS = document.getElementById('hideSOverlay');
 let offE = document.getElementById('hideEOverlay');
 let c1 = pickerS.color.hexString;
@@ -32,11 +34,13 @@ $(document).ready(function () {
         c1 = '#' + $(this).val().replace("#", "");
         pickerS.color.hexString = c1;
         b1.style.backgroundColor = c1;
+        lbl1.textContent = c1;
     });
     $("#hex2").on("input", function () {
         c2 = '#' + $(this).val().replace("#", "");
         pickerE.color.hexString = c2;
         b2.style.backgroundColor = c2;
+        lbl2.textContent = c2;
     });
     $(".offButton").on("click", function () {
         b1.style.backgroundColor = c1;
@@ -67,7 +71,7 @@ pickerS.on(['input:end'], function (color) {
     //colour1hex = color.hexString;
     b1.style.backgroundColor = color.hexString;
     c1 = color.hexString;
-
+    lbl1.textContent = c1;
 });
 
 pickerE.on(['input:end'], function (color) {
@@ -77,7 +81,7 @@ pickerE.on(['input:end'], function (color) {
     //colour1hex = color.hexString;
     b2.style.backgroundColor = color.hexString;
     c2 = color.hexString;
-
+    lbl2.textContent = c2;
 });
 
 function submit() {
