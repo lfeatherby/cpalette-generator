6cb4b6c717335c0b59cfeca3ea2c5c00cfe01b38
lfeatherby <larissafeatherby@gmail.com>
2024-11-03 14:31:03 -0500

    Update generator
    Added JSONP, labelling to generator files

--- generator/gScript.js
+++ generator/gScript.js
@@ -1,9 +1,12 @@
-const app = document.getElementById('root')
-var multi = document.getElementById('multi')
-var colorPickerS = new iro.ColorPicker('#pickerS');
-var currentColour = ''
-var currentMode = 'monochrome'
-var currentCount = '5'
+const app = document.getElementById("root");
+var multi = document.getElementById("multi");
+var pickerS = new iro.ColorPicker("#pickerS");
+var currentColour = "";
+var currentMode = "monochrome";
+var currentCount = "5";
+let b1 = document.getElementById("box1");
+let lbl1 = document.getElementById("lbl1");
+let c1 = pickerS.color.hexString;
 
 $(document).ready(function () {
   $(".s-nav button").click(function () {
@@ -14,18 +17,19 @@ $(document).ready(function () {
   $(".complement").click(function () {
     $(".num-nav button").removeClass("active");
     $(".two").addClass("active");
-    currentCount = $(".two").text().charAt(0);
+    currentCount = "2";
+    currentMode = "complement";
   });
   $(".triad").click(function () {
     $(".num-nav button").removeClass("active");
     $(".three").addClass("active");
-    currentCount = $(".three").text().charAt(0);
+    currentCount = "3";
   });
   $(".quad").click(function () {
     $(".num-nav button").removeClass("active");
     $(".four").addClass("active");
     console.log($(".four").text().charAt(0));
-    currentCount = $(".four").text().charAt(0);
+    currentCount = "4";
   });
   $("#resetALL").click(function () {
     $(":text").val("");
@@ -36,78 +40,67 @@ $(document).ready(function () {
     currentCount = $(this).text().charAt(0);
   });
   $(".navbar button").click(function () {
-    currentColour = colorPickerS.color.hexString.slice(1);
+    currentColour = pickerS.color.hexString.slice(1);
+    submit();
+  });
+  $("#hex1").on("input", function () {
+    c1 = $(this).val().replace("#", "");
+    pickerS.color.hexString = "#" + c1;
+    b1.style.backgroundColor = "#" + c1;
+    lbl1.textContent = "#" + c1;
+  });
+  $("#offButton").on("click", function () {
+    b1.style.backgroundColor = c1;
+    var o = $(".overlay");
+    o[0].style.display = "none";
     submit();
   });
 });
 
-colorPickerS.on(['input:end'], function (color) {
+function myDisplayFunction(myObj) {
+  for (var i = 0; i < myObj.count; i++) {
+    //console.log(data.count);
+    var l = document.createElement("p");
+    l.textContent = myObj.colors[i].hex["value"].toLowerCase();
+    l.style.color = myObj.contrast["value"];
+    l.classList.add("lbl");
+    var d = document.createElement("div");
+    d.appendChild(l);
+    d.classList.add("sq");
+    d.style.backgroundColor = myObj.colors[i].hex["value"];
+    multi.appendChild(d);
+    lbl1.style.color = myObj.contrast["value"];
+  }
+}
+pickerS.on(["input:end"], function (color) {
   // log the current color as a HEX string
-  var request = new XMLHttpRequest();
-  currentColour = color.hexString.slice(1);
-  multi.innerHTML = '';
+  c1 = color.hexString.slice(1);
+  multi.innerHTML = "";
+  lbl1.textContent = color.hexString;
+  b1.style.backgroundColor = color.hexString;
   // Open a new connection, using the GET request on the URL endpoint
   //currentMode = document.getElementById('mode').value
   //currentCount = document.getElementById('count').value
-  var full = 'https://www.thecolorapi.com/scheme?hex=' + currentColour + '&mode=' + currentMode + '&count=' + currentCount;
-  console.log(full);
-  request.open('GET', full, true)
-
-
-
-  request.onload = function () {
-    // Begin accessing JSON data here
-    var data = JSON.parse(this.response)
-    //console.log(data)
-
-    for (var i = 0; i < data.count; i++) {
-      //console.log(data.count);
-      var pic = document.createElement('img')
-      pic.src = data.colors[i].image['bare']
-      multi.appendChild(pic);
-    }
-  }
-
-  // Send request
-  request.send()
 });
 function submit() {
-  // Create a request variable and assign a new XMLHttpRequest object to it.
-  var request = new XMLHttpRequest()
-  // var x = event.target;
-  multi.innerHTML = ''
-  // Open a new connection, using the GET request on the URL endpoint
-  //currentMode = document.getElementById('mode').value
-  //currentCount = document.getElementById('count').value
-  var full = 'https://www.thecolorapi.com/scheme?hex=' + currentColour + '&mode=' + currentMode + '&count=' + currentCount;
-  console.log(full);
-  request.open('GET', full, true)
-
-  request.onload = function () {
-    // Begin accessing JSON data here
-    var data = JSON.parse(this.response)
-    //console.log(data)
-
-    for (var i = 0; i < data.count; i++) {
-      var pic = document.createElement('img')
-      pic.src = data.colors[i].image['bare']
-      multi.appendChild(pic);
-    }
-  }
-
-  // Send request
-  request.send()
-
+  multi.innerHTML = "";
+  let s = document.createElement("script");
+  s.src =
+    "https://www.thecolorapi.com/scheme?hex=" +
+    c1 +
+    "&format=jsonp&mode=" +
+    currentMode +
+    "&count=" +
+    currentCount +
+    "&callback=myDisplayFunction";
+  document.body.appendChild(s);
 }
 
 function on() {
   document.getElementById("overlay").style.display = "block";
-}
-
-function off() {
-  document.getElementById("overlay").style.display = "none";
+  multi.innerHTML = "";
 }
 
 function reset() {
-  multi.innerHTML = ''
+  multi.innerHTML = "";
 }
